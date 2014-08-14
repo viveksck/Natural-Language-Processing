2237d9cf2552db5df0f848c6c8fefd98b3f5fe67
Vivek Kulkarni <viveksck@gmail.com>
2014-08-14 19:46:31 +0530

    Works just fine

--- pa5-pcfg-v2/run.sh
+++ pa5-pcfg-v2/run.sh
@@ -1,12 +1,19 @@
 WORKING=./try/
-FREQB=90
-FREQW=$((100-$FREQB))
-CONTEXTO=30
-CONTEXTNEW=$((100-$CONTEXTO))
+FREQB='90 60 40 20 10'
+CONTEXTO='90 80 70 60 50 40 30 20 10'
 NUM=10000
-cp S1.gr $WORKING
-cp S2.gr $WORKING
-cp Vocab.gr $WORKING
-python ./skeleton.py --freqb $FREQB --freqw $FREQW --contexto $CONTEXTO --contextnew $CONTEXTNEW > $WORKING/$CONTEXTO.gr
-java -jar pcfg.jar generate -n $NUM $WORKING/*.gr > $WORKING/$CONTEXTO.ngrams
-
+for freqb in $FREQB
+do
+freqw=$((100-$freqb))
+mkdir -p $WORKING/$freqb
+cp S1.gr $WORKING/$freqb
+cp S2.gr $WORKING/$freqb
+cp Vocab.gr $WORKING/$freqb
+for contexto in $CONTEXTO
+do
+echo $freqb $contexto
+contextnew=$((100-$contexto))
+python ./skeleton.py --freqb $freqb --freqw $freqw --contexto $contexto --contextnew $contextnew > $WORKING/$freqb/$contexto.gr
+java -jar pcfg.jar generate -n $NUM $WORKING/$freqb/S1.gr $WORKING/$freqb/S2.gr $WORKING/$freqb/Vocab.gr $WORKING/$freqb/$contexto.gr > $WORKING/$freqb/$contexto.ngrams
+done
+done
